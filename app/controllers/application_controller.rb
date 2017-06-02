7f2c1c82229a136589c222c42953d6d505347fe2
Joshua Wolfe <jlwolfe1990@gmail.com>
2017-06-01 23:22:27 -0500

    Fixup the account edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   self.responder = ApplicationResponder
   respond_to :html
 
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -6,22 +6,37 @@ feature 'Edit account', type: :feature, js: true do
       FactoryGirl.create :user, password: 'fakepass', password_confirmation: 'fakepass'
     end
 
+    let :password do
+      'fakepass'
+    end
+
+    let :new_name do
+      'Josh Wolfe'
+    end
+
     before do
-      signin user.email, 'fakepass'
+      signin user.email, password
     end
 
     it 'should be able to create a topic request' do
-      visit root_path
+      find('.js-user-dropdown').click
 
-      expect(page).to have_css('.js-new-topic-request')
-      find('.js-new-topic-request').click
+      click_on 'Edit account'
 
-      fill_in('topic_request_title', with: 'TR1')
-      fill_in('topic_request_description', with: 'TR1 Desc')
+      expect(page).to have_content('Edit User')
 
-      click_on 'Create Topic request'
+      fill_in 'user_name', with: new_name
+      fill_in 'user_current_password', with: password
 
-      expect(page).to have_current_path(root_path)
+      click_on 'Update'
+
+      find('.js-user-dropdown').click
+
+      click_on 'Edit account'
+
+      expect(page).to have_content('Edit User')
+
+      expect(find_field('user_name').value).to eq(new_name)
     end
   end
 end
