c217079bd03c641b201caa2e1fcc0f372882e63a
Joshua Wolfe <jlwolfe1990@gmail.com>
2017-09-09 16:22:31 -0500

    wip

--- config/application.rb
+++ config/application.rb
@@ -30,11 +30,11 @@ module ReciprocateLife
 
     config.paperclip_defaults = {
       storage: :s3,
-      s3_region: ENV.fetch('AWS_REGION'),
+      s3_region: ENV['AWS_REGION'],
       s3_credentials: {
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        bucket: ENV.fetch('S3_BUCKET_NAME')
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        bucket: ENV['S3_BUCKET_NAME']
       },
       url: ':s3_domain_url',
       path: '/:class/:attachment/:id_partition/:style/:filename'
