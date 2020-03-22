ac761241101b7069b750b587dbb2b556a13b50eb
Joshua Wolfe <jlwolfe1990@gmail.com>
2020-03-22 17:44:49 -0500

    Force ssl from controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_to_https
+
   self.responder = ApplicationResponder
   respond_to :html
 
@@ -13,5 +15,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def redirect_to_https
+    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
   end
 end
